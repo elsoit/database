ff36bf239bf38da1a2e66ef817c58c0a9af8528c
elsoit <sayedalorabi@hotmail.com>
2022-05-03 13:21:25 +0200

    query2

--- Programmatori.sql
+++ Programmatori.sql
@@ -1,6 +1,15 @@
 /*
                  --QUERY 1 --
-select distinct codice
-from Programma join Autore on Programma.id = Autore.id 
+select distinct codice , java , anno 
+from Programma join Autore on Programma.codice = Auotore.id 
 where Programma.linguaggio = "java" and Programma.anno>2000;
 */
+
+/*
+                 --QUERY 2 --
+select distinct nome, categoria, linguaggio
+from Programmatore join Autore on Programmatore.codice=Autore.codice join Programma on Autore.id = Programma.id
+where linguaggio !="Python" 
+Order by  nome ;
+*/
+
