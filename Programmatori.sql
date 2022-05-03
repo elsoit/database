9406ea0d89181ea940486a84cbe40ccac16b33c9
elsoit <sayedalorabi@hotmail.com>
2022-05-03 14:38:24 +0200

    query3

--- Programmatori.sql
+++ Programmatori.sql
@@ -13,3 +13,11 @@ where linguaggio !="Python"
 Order by  nome ;
 */
 
+/*
+                 --QUERY 3 --
+select  Programmatore.codice, min(anno)
+from Programmatore join Autore on Programmatore.codice=Autore.codice join Programma on Autore.id = Programma.id
+where linguaggio !="Java " and categoria = 10
+group by Programmatore.codice;
+*/
+
